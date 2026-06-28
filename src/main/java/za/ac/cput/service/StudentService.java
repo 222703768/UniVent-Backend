374955a77205abdd36c0d8371fc21376934ca24f
samthepessimist <231116144@mycput.ac.za>
2026-06-28 20:40:39 +0200

    added student service implementation class

---
+++ src/main/java/za/ac/cput/service/StudentService.java
@@ -0,0 +1,30 @@
+package za.ac.cput.service;
+
+import za.ac.cput.domain.Student;
+
+public class StudentService implements IStudentService{
+    @Override
+    public Student findByStudentNumber(String studentNumber) {
+        return null;
+    }
+
+    @Override
+    public Student create(Student student) {
+        return null;
+    }
+
+    @Override
+    public Student read(String string) {
+        return null;
+    }
+
+    @Override
+    public Student update(Student student) {
+        return null;
+    }
+
+    @Override
+    public void delete(String string) {
+
+    }
+}
